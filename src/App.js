0919109e6fdcd9d39b2490da6c9770c7305ce051
Helio Alves <hellius.af@gmail.com>
2021-09-30 19:34:46 -0300

    Criação do header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React from 'react';
 import './App.css'
-import Api from './api/Api';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/login/Login';
+
 
 
 function App() {
-  return <div> <Api /> </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Login" element={<Login />} />
+          </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
