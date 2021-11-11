ecf1b59ed3b666176dc1e1c41d7ff8ab6dce893b
Helio Alves <hellius.af@gmail.com>
2021-11-10 21:22:26 -0300

    Correção no token de autenticação

--- src/components/login/LoginCreate.js
+++ src/components/login/LoginCreate.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { USER_POST } from '../../api';
+import useFetch from '../../hooks/useFetch';
 import useForm from '../../hooks/useForm';
 import UserContext from '../../UserContext';
 import Button from '../forms/Button';
 import Input from '../forms/Input';
+import Error from '../helper/Error';
+
 
 const LoginCreate = () => {
 
@@ -12,6 +15,7 @@ const LoginCreate = () => {
     const password =useForm('');
 
     const {userLogin} = React.useContext(UserContext);
+    const { loading, error, request } = useFetch();
 
     async function handleSubmit (event){
         event.preventDefault();
@@ -21,10 +25,9 @@ const LoginCreate = () => {
             email: email.value,
             password: password.value,
 
-        })
-        const response = await fetch(url, options);
+        });
+        const { response } = await request(url, options);
         if (response.ok) userLogin(username.value, password.value);
-        
     }
 
     return (
@@ -35,7 +38,8 @@ const LoginCreate = () => {
                 <Input label="Usuário" type="text" name="username" {...username}/>
                 <Input label="Email" type="email" name="email" {...email}/>
                 <Input label="Senha" type="password" name="username" {...password}/>
-                <Button>Cadastrar</Button>
+                {loading ? <Button disabled>Cadastrando...</Button> : <Button>Cadastrar</Button>}
+                <Error error={error}/>
             </form>
 
         </section>
