32201b2c0c6861c0903b2d5a2fbf0b09b4238da6
Helio Alves <hellius.af@gmail.com>
2023-02-13 08:39:22 -0300

    Componentes finalizados feed e fotos

--- src/components/user/UserPhotoPost.js
+++ src/components/user/UserPhotoPost.js
@@ -1,10 +1,82 @@
 import React from 'react'
+import styles from './UserPhotoPost.module.css'
+import useForm from '../../hooks/useForm'
+import useFetch from '../../hooks/useFetch'
+import Input from '../forms/Input'
+import Button from '../forms/Button'
+import Error from '../helper/Error'
+import { PHOTO_POST } from '../../api'
+import { useNavigate } from 'react-router-dom'
+
+
 
 const UserPhotoPost = () => {
+
+    const nome = useForm()
+    const peso = useForm()
+    const idade = useForm()
+    const [img, setImg] = React.useState({})
+    const {data, error, loading, request} = useFetch()
+    const navigate = useNavigate()
+
+    const handleSubmit = (event) => {
+        
+            event.preventDefault()
+            const formData = new FormData()
+            formData.append('img', img.raw)
+            formData.append('nome', nome.raw)
+            formData.append('peso', peso.raw)
+            formData.append('idade', idade.raw)
+
+            const token = window.localStorage.getItem('token')
+            const {url, options} = PHOTO_POST(formData, token)
+            request(url, options)
+        
+    }
+    
+    const handleImgChange = ({target}) => {
+        setImg({
+            preview: URL.createObjectURL(target.files[0]),
+            raw: target.files[0],
+        })
+    
+    }
+
+    React.useEffect(() =>{
+        if(data) navigate('/conta')
+    },[data, navigate])
+
     return (
-        <div>
-            Postar foto
-        </div>
+        <section className={`${styles.photoPost} animeLeft` }>
+            <form onSubmit={handleSubmit}>
+                <Input label="Nome" type="text" name="nome" {...nome}/>
+                <Input label="Peso" type="number" name="peso" {...peso}/>
+                <Input label="Idade" type="number" name="idade" {...idade}/>
+                <Input
+                  className={styles.file}
+                  type="file" 
+                  name="img" 
+                  onChange={handleImgChange}
+                  />
+                {
+                    loading ? (
+                        <Button disabled>Enviando...</Button>
+                    ): (
+                        <Button>Enviar</Button>
+                    )
+                }
+                <Error error={error} />
+               
+            </form>
+            <div>
+                {img.preview && (
+                    <div className={styles.preview}
+                    style={{backgroundImage: `url('${img.preview}')`}}
+                    ></div>
+                )}
+
+            </div>
+        </section>
     )
 }
 
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -6,6 +6,15 @@ const types = {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      "A senha deve ter no mínimo 8 caracteres, 1 caracter maiúsculo, 1 caracter minúsculo e 1 digito.",
+  },
+  number: {
+    regex: /^\d+$/,
+    message: "Utilize apenas números.",
+  },
 };
 
 
